36faf3d7edbdd441233504be37446fa5499d7819
roddg86 <roddg@mail.ru>
2022-08-17 11:39:28 +0900

    Notifications

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 /* AppCompatActivity Это подкласс, наследующий от класса Android Activity и
 обеспечивающий поддержку старых версий Android. */
@@ -25,12 +26,14 @@ class MainActivity : AppCompatActivity() {
 
         /* Слушатель щелчок кнопки */
         trueButton.setOnClickListener { view: View ->
-            // Блок выполнения кода после нажантия
+            /* Создание уведомления по нажатию кнопки */
+            Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_SHORT).show()
         }
 
         /* Слушатель для кнопки FALSE */
         falseButton.setOnClickListener { view: View ->
-            //Блок выполнения кода после нажатия
+            /* Создание уведолния по нажатию кнопки */
+            Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show()
         }
     }
 }
