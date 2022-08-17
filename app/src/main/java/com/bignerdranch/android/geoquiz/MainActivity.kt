f26fe3a32dee4d7112877034b51270b1567e905a
roddg86 <roddg@mail.ru>
2022-08-17 21:29:27 +0900

    Orientation change

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,10 +2,8 @@ package com.bignerdranch.android.geoquiz
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
 import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -16,11 +14,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: ImageButton
-    private lateinit var prevButton: ImageButton
+    private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
 
-    /* Создадим список обьектов вопросов */
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
         Question(R.string.question_oceans, true),
@@ -33,7 +29,6 @@ class MainActivity : AppCompatActivity() {
     private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
@@ -41,7 +36,6 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
-        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -52,31 +46,17 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
 
-        /* Слушатель для кнопки Next */
-        nextButton.setOnClickListener { view: View ->
-            nextQuestion()
-            updateQuestion()
-        }
-
-        /* Добавление кнопки возврата */
-        prevButton.setOnClickListener { viev: View ->
-            prevQuestion()
-            updateQuestion()
-        }
-
-        /* слушатель для TextView */
-        questionTextView.setOnClickListener { view: View ->
-            nextQuestion()
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
 
         updateQuestion()
     }
 
-    /* Переопределим функции жизненного цикла MainActivity */
     override fun onStart() {
         super.onStart()
-        Log.d(TAG,"onStart() called")
+        Log.d(TAG, "onStart() called")
     }
 
     override fun onResume() {
@@ -91,45 +71,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
-        Log.d(TAG,"onStop() called")
+        Log.d(TAG, "onStop() called")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG,"onDestroy() called")
+        Log.d(TAG, "onDestroy() called")
     }
 
-    /* Инкапсуляция с помощью функции */
     private fun updateQuestion() {
-        /* Отобразим вопрос из списка */
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }
 
-    /* Функция получает логическую переменную, которая
-    указывает, какую кнопку нажал пользователь: TRUE или FALSE.
-    Ответ пользователя проверяется по ответу текущего объекта
-    Question. Наконец, после определения правильности ответа
-    функция создает Toast для вывода соответствующего
-    сообщения. */
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
-
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
         } else {
             R.string.incorrect_toast
         }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
-    }
-
-    /* функция следующего вопроса */
-    private fun nextQuestion() {
-        currentIndex = (currentIndex + 1) % questionBank.size
-    }
-
-    /* функция возвращение к предыдущему вопросу */
-    private fun prevQuestion() {
-        currentIndex = (currentIndex - 1) % questionBank.size
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show()
     }
 }
