504584eb0210f045219877cf7af9421c17e45886
roddg86 <roddg@mail.ru>
2022-08-17 12:51:37 +0900

    Notifications api 28

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.bignerdranch.android.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 /* AppCompatActivity Это подкласс, наследующий от класса Android Activity и
 обеспечивающий поддержку старых версий Android. */
@@ -27,13 +28,17 @@ class MainActivity : AppCompatActivity() {
         /* Слушатель щелчок кнопки */
         trueButton.setOnClickListener { view: View ->
             /* Создание уведомления по нажатию кнопки */
-            Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_SHORT).show()
+            val toast = Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP,0,160)
+            toast.show()
         }
 
         /* Слушатель для кнопки FALSE */
         falseButton.setOnClickListener { view: View ->
             /* Создание уведолния по нажатию кнопки */
-            Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show()
+            val toast = Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP or Gravity.LEFT, 0, 160)
+            toast.show()
         }
     }
 }
