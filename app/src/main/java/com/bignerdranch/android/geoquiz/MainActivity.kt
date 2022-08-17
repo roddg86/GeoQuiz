5e076fe2fad5c2b85c0b3975388419e7f04b5220
roddg86 <roddg@mail.ru>
2022-08-17 14:01:01 +0900

    MVC - controller level

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -4,41 +4,60 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-/* AppCompatActivity Это подкласс, наследующий от класса Android Activity и
-обеспечивающий поддержку старых версий Android. */
 class MainActivity : AppCompatActivity() {
-    /* указать компилятору, что мы введём ненулевое значение
-    View перед попыткой использовать содержимое свойства. */
+
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nestButton: Button
+    private lateinit var questionTextView: TextView
+
+    /* Создадим список обьектов вопросов */
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        /* объекты не появляются в памяти до тех пор, пока в onCreate(...) не будет вызвана
-        функция setContentView(...) */
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* Ищем и назначаем обьектам представления соответствующие свойства */
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nestButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
-        /* Слушатель щелчок кнопки */
         trueButton.setOnClickListener { view: View ->
-            /* Создание уведомления по нажатию кнопки */
-            val toast = Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.TOP,0,160)
+            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 160)
             toast.show()
         }
 
-        /* Слушатель для кнопки FALSE */
         falseButton.setOnClickListener { view: View ->
-            /* Создание уведолния по нажатию кнопки */
-            val toast = Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_SHORT)
+            val toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
             toast.setGravity(Gravity.TOP or Gravity.LEFT, 0, 160)
             toast.show()
         }
+
+        /* Слушатель для кнопки Next */
+        nestButton.setOnClickListener { view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            val questionTextResId = questionBank[currentIndex].textResId
+            questionTextView.setText(questionTextResId)
+        }
+
+        /* Отобразим вопрос из списка */
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
     }
 }
