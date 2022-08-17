f49c6aa594fa795359bee9b1a57b2ef3f6a13814
roddg86 <roddg@mail.ru>
2022-08-18 03:28:37 +0900

    ViewModel

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -19,17 +19,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
-    )
-
-    private var currentIndex = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
@@ -8,15 +8,27 @@ private const val TAG = "QuizViewModel"
 /* свой подкласс ViewModel */
 /* QuizViewModel не уничтожается при повороте, и MainActivity сохраняет данные о состоянии
 интерфейса своей activity в экземпляре QuizViewModel, который после поворота тоже сохраняется */
-class QuizViewModel:ViewModel() {
-    init {
-        Log.d(TAG,"ViewModel instance created")
-    }
+/* ViewModel хранит все данные, связанные с потребностями экрана */
+class QuizViewModel : ViewModel() {
+    var currentIndex = 0
 
-    /* Функция onCleared() вызывается непосредственно перед
-    уничтожением View Model */
-    override fun onCleared() {
-        super.onCleared()
-        Log.d(TAG,"ViewModel instance about to be destroyed")
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    /* Добавление бизнес-логики */
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
     }
 }
