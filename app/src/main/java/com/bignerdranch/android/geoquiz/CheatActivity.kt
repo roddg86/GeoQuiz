a9469e9a6023565d0edb7bfaf1d4fd5e1bab037c
roddg86 <roddg@mail.ru>
2022-08-18 16:53:23 +0900

    Peeping at the answers

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
@@ -3,11 +3,15 @@ package com.bignerdranch.android.geoquiz
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 private const val EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
+    private lateinit var answerTextView: TextView
+    private lateinit var showAnswerButton: Button
     private var answerIsTrue = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +21,16 @@ class CheatActivity : AppCompatActivity() {
         /* чтения значения из дополнения */
         answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
 
+        /* Добавление функциональности подсматривания ответов */
+        answerTextView = findViewById(R.id.answer_text_view)
+        showAnswerButton = findViewById(R.id.show_answer_button)
+        showAnswerButton.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            answerTextView.setText(answerText)
+        }
     }
 
     /* инскапсуляция функции */
