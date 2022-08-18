aa299ff34041758c74c3b65e6b59031b774ffd8e
roddg86 <roddg@mail.ru>
2022-08-18 11:30:20 +0900

    Save state

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.geoquiz
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -11,6 +12,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 
 private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var questionTextView: TextView
 
     /* Ленивая инициализация QuizViewModel */
-    private val quizViewModel: QuizViewModel by lazy{
+    private val quizViewModel: QuizViewModel by lazy {
         ViewModelProviders.of(this).get(QuizViewModel::class.java)
     }
 
@@ -28,6 +30,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
+
+        /* Проверка наборов в OnCreate */
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX,0)?:0
+        quizViewModel.currentIndex = currentIndex
 
         /* Cвяжем activity с экземпляром QuizViewModel. */
         val provider: ViewModelProvider = ViewModelProviders.of(this)
@@ -69,6 +75,13 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause() called")
+    }
+
+    /* Переопределение onSaveInstanceState */
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX,quizViewModel.currentIndex)
     }
 
     override fun onStop() {
